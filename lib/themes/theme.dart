2601c638e66e120c1ea304d12c4792c9ad7abda3
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-23 10:28:44 +0300

    my custom themes

--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
-ThemeData lightMode =
-    ThemeData(colorScheme: ColorScheme.light(surface: Colors.grey.shade200));
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+      surface: Colors.grey.shade200,
+      primary: Colors.blue.shade900,
+      secondary: Colors.blue.shade900,
+      tertiary: Colors.blue.shade50,
+      inversePrimary: Colors.white,)
+);
 ThemeData darkMode = ThemeData.dark();
