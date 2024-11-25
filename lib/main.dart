56e7481f5a110d12f8db1e7b3580ebff32c9074c
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-25 16:30:41 +0300

    root file

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:messaging_app/pages/login_page.dart';
+import 'package:messaging_app/themes/light_mode.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeData currentTheme = lightMode;
+
+  void updateTheme(ThemeData theme) {
+    setState(() {
+      currentTheme = theme;
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: lightMode,
       title: 'Messaging App',
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
