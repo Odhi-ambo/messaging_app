ef839ff3af6f2c60badb40ff44ccf87ab57e92e1
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-25 10:17:17 +0300

    light mode

---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  surface: Colors.grey.shade800,
+  primary: Colors.blue.shade900,
+  secondary: Colors.blue.shade900,
+  tertiary: Colors.blue.shade50,
+  inversePrimary: Colors.white,
+));
+/*ThemeData darkMode = ThemeData(
+    colorScheme: ColorScheme.dark(
+  surface: Colors.grey.shade900,
+  primary: Colors.blue.shade900,
+  secondary: Colors.blue.shade900,
+  tertiary: Colors.blue.shade50,
+  inversePrimary: Colors.white,
+)
+);*/
