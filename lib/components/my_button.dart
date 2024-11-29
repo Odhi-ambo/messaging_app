20b4b3de3b860e127fcdbfb4ef7986f4b214311b
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-30 00:46:14 +0300

    my custom buttons

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class MyButton extends StatelessWidget {
+  final Function() onTap;
   final String text;
-  const MyButton({super.key, required this.text});
+  const MyButton({super.key, required this.text,required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      padding: EdgeInsets.all(15),
-      margin: EdgeInsets.symmetric(
-        horizontal: 25,
-      ),
-      child: Center(child:Text (text),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.symmetric(
+          horizontal: 25,
+        ),
+        child: Center(child:Text (text),
+        ),
       ),
     );
   }
