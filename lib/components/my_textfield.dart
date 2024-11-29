d56de778a6852c82b2bc23360745679ecce74b7b
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-29 23:59:04 +0300

    my custom textfield file

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class MyTextfield extends StatelessWidget {
   final bool obscureText;
+  final TextEditingController controller;
   final String hintText;
   const MyTextfield(
-      {super.key, required this.hintText, required this.obscureText});
+      {super.key, required this.hintText, required this.obscureText, required this.controller});
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +13,7 @@ class MyTextfield extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: TextField(
         obscureText: obscureText,
+        controller: controller,
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(15),
