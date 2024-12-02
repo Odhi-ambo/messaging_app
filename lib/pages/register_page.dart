260bc8e322a0c7d12a8a45fa2c26698def417a9f
Odhi-ambo <michaelderrick605@gmail.com>
2024-12-02 11:16:22 +0300

    registration page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -5,6 +5,8 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(child: Column()),
+    );
   }
 }
