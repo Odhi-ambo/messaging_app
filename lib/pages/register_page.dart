093ff738b6157889dfb2b0ee84961b83bcb877eb
Odhi-ambo <michaelderrick605@gmail.com>
2024-12-02 11:30:05 +0300

    registration page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -15,12 +15,18 @@ class RegisterPage extends StatelessWidget {
                   size: 100,
                   color: Theme.of(context).colorScheme.inversePrimary),
             ),
+            const SizedBox(
+              height: 50,
+            ),
 
             //welcome msg
             Text(
               "Let's get started!",
               style: TextStyle(
                   color: Theme.of(context).primaryColor, fontSize: 18),
+            ),
+            const SizedBox(
+              height: 15,
             )
 
             //email textfield
