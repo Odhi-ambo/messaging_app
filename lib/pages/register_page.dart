186e4320951ad792f6716cccd23f696c5fc55f76
Odhi-ambo <michaelderrick605@gmail.com>
2024-12-06 12:10:57 +0300

    registration page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -26,69 +26,75 @@ class RegisterPage extends StatelessWidget {
         ),
         SafeArea(
           child: SingleChildScrollView(
-            child: Container(
-              height: MediaQuery.of(context).size.height,
-              
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  //logo
-                  children: [
-                    Center(
-                      child: Icon(Icons.message,
-                          size: 100,
-                          color: Theme.of(context).colorScheme.secondary),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-
-                    //welcome msg
-                    Text(
-                      "Let's get started!",
-                      style: TextStyle(
-                          color: Theme.of(context).primaryColor, fontSize: 18),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-
-                    //email textfield
-                    MyTextfield(
-                        hintText: 'Enter email..',
-                        obscureText: false,
-                        controller: emailController),
-
-                    //password textfield
-                    MyTextfield(
-                        hintText: 'Enter password..',
-                        obscureText: true,
-                        controller: passwordController),
-
-                    //confirm password textfield
-                    MyTextfield(
-                        hintText: 'Confirm password..',
-                        obscureText: true,
-                        controller: confirmPasswordController),
-
-                    //register button
-                    MyButton(text: 'Register', onTap: register),
-                    const SizedBox(
-                      height: 25,
-                    ),
-
-                    //login button
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                      Text('Already have an account?',
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontSize: 15)),
-                      GestureDetector(
-                        onTap: onTap,
-                        child: const Text(' Login here!',
-                            style: TextStyle(color: Colors.blue, fontSize: 15)),
+            child: SizedBox(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    //logo
+                    children: [
+                      Center(
+                        child: Icon(Icons.message,
+                            size: 100,
+                            color: Theme.of(context).colorScheme.secondary),
                       ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+
+                      //welcome msg
+                      Text(
+                        "Let's get started!",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 18),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+
+                      //email textfield
+                      MyTextfield(
+                          hintText: 'Enter email..',
+                          obscureText: false,
+                          controller: emailController),
+
+                      //password textfield
+                      MyTextfield(
+                          hintText: 'Enter password..',
+                          obscureText: true,
+                          controller: passwordController),
+
+                      //confirm password textfield
+                      MyTextfield(
+                          hintText: 'Confirm password..',
+                          obscureText: true,
+                          controller: confirmPasswordController),
+
+                      //register button
+                      MyButton(text: 'Register', onTap: register),
+                      const SizedBox(
+                        height: 25,
+                      ),
+
+                      //login button
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Already have an account?',
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    fontSize: 15)),
+                            GestureDetector(
+                              onTap: onTap,
+                              child: const Text(' Login here!',
+                                  style: TextStyle(
+                                      color: Colors.blue, fontSize: 15)),
+                            ),
+                          ]),
                     ]),
-                  ]),
+              ),
             ),
           ),
         ),
